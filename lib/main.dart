225546f03bb9913aa18eef1693a1eb4959d1151b
SftIce <workshop@cateringequipment.co.za>
2026-04-21 10:30:56 +0200

    Refactor main function to use DartsApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 import 'game_selection.dart';
 
-void main() => runApp(const MaterialApp(home: GameSelection(), debugShowCheckedModeBanner: false));
+void main() => runApp(const DartsApp());
+
+class DartsApp extends StatelessWidget {
+  const DartsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Darts Scoring App',
+      theme: ThemeData.dark(),
+      home: GameSelection(),
+    );
+  }
+}
